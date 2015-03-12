d81c9011235ba5696b74f0df9b6cada87621336d
wangjie <wangjie.magic@gmail.com>
2015-03-12 16:50:09 +0800

    线性表单链表实现；单元测试；线性表抽象数据结构调整

---
+++ Test.MagicBox/SingleLinkListTest.cs
@@ -0,0 +1,75 @@
+﻿using MagicBox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.MagicBox
+{
+    [TestClass]
+    public class SingleLinkListTest
+    {
+        private SingleLinkList<int> singleLinkList=new SingleLinkList<int>(0, 1, 2, 3, 4);
+        [TestMethod]
+        public void ThisTest()
+        {
+            Assert.AreEqual(0,singleLinkList[0]);
+        }
+        [TestMethod]
+        public void LastText()
+        {
+            Assert.AreEqual(singleLinkList.Last,4);
+        }
+        [TestMethod]
+        public void CountText()
+        {
+            Assert.AreEqual(singleLinkList.Count, 5);
+        }
+        [TestMethod]
+        public void GetElementText()
+        {
+            Assert.AreEqual(singleLinkList.GetElement(4), 4);
+        }
+        [TestMethod]
+        public void IndexOfText()
+        {
+            Assert.AreEqual(singleLinkList.IndexOf(4), 4);
+        }
+        [TestMethod]
+        public void PreElementText()
+        {
+            Assert.AreEqual(singleLinkList.PreElement(4), 3);
+        }
+        [TestMethod]
+        public void NextElementText()
+        {
+            Assert.AreEqual(singleLinkList.NextElement(3), 4);
+        }
+        [TestMethod]
+        public void AppendText()
+        {
+            singleLinkList.Append(5);
+            Assert.AreEqual(singleLinkList.Last, 5);
+        }
+        [TestMethod]
+        public void InsertText()
+        {
+            singleLinkList.Insert(0,6);
+            Assert.AreEqual(singleLinkList[0],6);
+        }
+        [TestMethod]
+        public void RemoveAtText()
+        {
+            singleLinkList.RemoveAt(0);
+            Assert.AreEqual(singleLinkList[0], 1);
+        }
+        [TestMethod]
+        public void ToStringText()
+        {
+            Assert.AreEqual(singleLinkList.ToString(), "0,1,2,3,4");
+        }
+        [TestMethod]
+        public void ReverseText()
+        {
+            singleLinkList.Reverse();
+            Assert.AreEqual(singleLinkList.ToString(), "4,3,2,1,0");
+        }
+    }
+}
