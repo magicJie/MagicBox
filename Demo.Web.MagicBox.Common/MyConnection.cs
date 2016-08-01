896651ce53417a43cee6677029d972c53ed92252
wangjie <wangjie.magic@gmail.com>
2016-08-02 03:55:25 +0800

    signalR

---
+++ Demo.Web.MagicBox.Common/MyConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Demo.Web.MagicBox.Common
+{
+    public class MyConnection:PersistentConnection
+    {
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            data = string.Format("数据是：{0} 时间是：{1}", data, DateTime.Now.ToString());
+            return Connection.Send(connectionId, data);
+        }
+    }
+}
